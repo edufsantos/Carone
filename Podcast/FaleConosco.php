125b3f26cebbe60e5defbff1099c09e20f296e6f
EduardoDev <53874616+edufsantos@users.noreply.github.com>
2019-12-08 15:24:13 -0300

    Add files via upload

---
+++ Podcast/FaleConosco.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+	<head>
+		<meta http-equiv="content-type" content="text/html">
+		<meta charset="utf-8">
+		<meta name="author" content="Enzo Paiva">
+		<meta name="copyright" content="IFMS">
+		<meta name="keywords" content="página de conteúdo educacional">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+		<link rel="stylesheet" type="text/css" href="Estilo_PI.css"/>
+    <link rel="stylesheet" type="text/css" href="FaleConosco.css"/>
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+  </head>
+
+  <body background="Design.png">
+		<div class="container">
+			<?php include 'menu.php';?>
+
+			<div id="conteudoo">
+        <div class="info row">
+					<div class="col-12">
+						<h1>Fale Conosco</h1>
+	          <p>Entre em contato conosco para esclarecer dúvidas, relatar erros, requerir nosso mídia kit, dar sugestões,
+							fazer críticas construtivas ou elogios!
+	          <br>
+					</div>
+					<div class="col-12">
+		        <h2>Facebook</h2>
+	          <a  href="https://www.facebook.com/ScienceTalkPodcastIF/" target="_blank">https://www.facebook.com/ScienceTalkPodcastIF/</a> <br>
+	          <br><br>
+					</div>
+					<div class="col-12">
+	          <h2>Twitter</h2>
+	          <a  href="https://twitter.com/ScienceTalkIF" target="_blank">https://twitter.com/ScienceTalkIF</a>
+	          <br><br>
+					</div>
+					<div class="col-12">
+	          <h2>E-mail</h2>
+	          <p>beta.science.talk@gmail.com</p>
+					</div>
+        </div>
+		</div>
+		<footer class="footer">
+			<?php include 'rodape.php';?>
+		</footer>
+  </body>
+</html>
